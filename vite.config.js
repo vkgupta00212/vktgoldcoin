25e3d80485236c3712c3c41039d918aab70b65e5
Vishal Kumar <vkgupta00212@gmail.com>
2025-07-12 12:39:23 +0530

    vercel changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vktgoldcoin/',  // 👈 This must match your GitHub repo name
+  base: '/',  // 👈 This must match your GitHub repo name
   plugins: [react()],
 })
